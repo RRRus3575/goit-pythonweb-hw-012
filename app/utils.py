c9ecc94a00def0b34a582c9559416ab2de7d4739
Ruslan <ruslan.grudistov@gmail.com>
2025-02-05 14:17:15 +0200

    reset

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,6 @@ import smtplib
 from email.mime.text import MIMEText
 import os
 
-# Переменные из .env
 SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = os.getenv("SMTP_PORT")
 SMTP_USER = os.getenv("SMTP_USER")
@@ -11,7 +10,7 @@ SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 if not all([SMTP_SERVER, SMTP_PORT, SMTP_USER, SMTP_PASSWORD]):
     raise ValueError("❌ SMTP настройки не заданы в .env")
 
-# Отправка письма
+# Функция для отправки письма подтверждения
 def send_verification_email(subject, body, to_email):
     try:
         msg = MIMEText(body)
@@ -27,5 +26,22 @@ def send_verification_email(subject, body, to_email):
     except Exception as e:
         print(f"❌ Ошибка при отправке email: {e}")
 
+# Функция для отправки письма сброса пароля
+def send_reset_email(to_email: str, reset_link: str):
+    try:
+        subject = "Сброс пароля"
+        body = f"Для сброса пароля перейдите по ссылке: {reset_link}"
+        
+        msg = MIMEText(body)
+        msg["Subject"] = subject
+        msg["From"] = SMTP_USER
+        msg["To"] = to_email
 
+        with smtplib.SMTP(SMTP_SERVER, int(SMTP_PORT)) as server:
+            server.starttls()
+            server.login(SMTP_USER, SMTP_PASSWORD)
+            server.sendmail(SMTP_USER, to_email, msg.as_string())
 
+        print("✅ Письмо для сброса пароля успешно отправлено!")
+    except Exception as e:
+        print(f"❌ Ошибка при отправке email: {e}")
