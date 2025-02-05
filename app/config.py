f0ed3a407a9f22f8a5ef544f3a7949fdec2a0961
Ruslan <ruslan.grudistov@gmail.com>
2025-02-05 15:11:42 +0200

    1

--- app/config.py
+++ app/config.py
@@ -1,18 +1,19 @@
 import os
-import redis
 from dotenv import load_dotenv
+from pydantic import BaseSettings
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env"))
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = os.getenv("SMTP_PORT")
-SMTP_USER = os.getenv("SMTP_USER")
-SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
-CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
-CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
-CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+class Settings(BaseSettings):
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    SMTP_SERVER: str = os.getenv("SMTP_SERVER")
+    SMTP_PORT: int = int(os.getenv("SMTP_PORT", 587))
+    SMTP_USER: str = os.getenv("SMTP_USER")
+    SMTP_PASSWORD: str = os.getenv("SMTP_PASSWORD")
+    CLOUDINARY_CLOUD_NAME: str = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY: str = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET: str = os.getenv("CLOUDINARY_API_SECRET")
+    REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
-redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+settings = Settings()
