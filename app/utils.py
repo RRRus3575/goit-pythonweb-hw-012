93eef9e3d9790b80b20cedf262ce628433547cb9
Ruslan <ruslan.grudistov@gmail.com>
2025-02-05 15:33:49 +0200

    1

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
 import os
+from aiosmtplib import send
 
 SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = os.getenv("SMTP_PORT")
@@ -11,36 +12,50 @@ if not all([SMTP_SERVER, SMTP_PORT, SMTP_USER, SMTP_PASSWORD]):
     raise ValueError("❌ SMTP настройки не заданы в .env")
 
 # Функция для отправки письма подтверждения
-def send_verification_email(subject, body, to_email):
-    try:
-        msg = MIMEText(body)
-        msg["Subject"] = subject
-        msg["From"] = SMTP_USER
-        msg["To"] = to_email
+async def send_verification_email(subject, body, to_email):
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = os.getenv("SMTP_USER")
+    msg["To"] = to_email
 
-        with smtplib.SMTP(SMTP_SERVER, int(SMTP_PORT)) as server:
-            server.starttls()
-            server.login(SMTP_USER, SMTP_PASSWORD)
-            server.sendmail(SMTP_USER, to_email, msg.as_string())
+    try:
+        await send(
+            msg,
+            hostname=os.getenv("SMTP_SERVER"),
+            port=int(os.getenv("SMTP_PORT")),
+            username=os.getenv("SMTP_USER"),
+            password=os.getenv("SMTP_PASSWORD"),
+            start_tls=True,
+        )
         print("✅ Email успешно отправлен!")
     except Exception as e:
         print(f"❌ Ошибка при отправке email: {e}")
 
 # Функция для отправки письма сброса пароля
-def send_reset_email(to_email: str, reset_link: str):
+async def send_reset_email(to_email: str, reset_link: str):
+    """
+    Asynchronously send a password reset email.
+
+    :param to_email: Recipient email.
+    :param reset_link: Password reset link.
+    """
     try:
         subject = "Сброс пароля"
         body = f"Для сброса пароля перейдите по ссылке: {reset_link}"
-        
+
         msg = MIMEText(body)
         msg["Subject"] = subject
-        msg["From"] = SMTP_USER
+        msg["From"] = os.getenv("SMTP_USER")
         msg["To"] = to_email
 
-        with smtplib.SMTP(SMTP_SERVER, int(SMTP_PORT)) as server:
-            server.starttls()
-            server.login(SMTP_USER, SMTP_PASSWORD)
-            server.sendmail(SMTP_USER, to_email, msg.as_string())
+        await send(
+            msg,
+            hostname=os.getenv("SMTP_SERVER"),
+            port=int(os.getenv("SMTP_PORT")),
+            username=os.getenv("SMTP_USER"),
+            password=os.getenv("SMTP_PASSWORD"),
+            start_tls=True,
+        )
 
         print("✅ Письмо для сброса пароля успешно отправлено!")
     except Exception as e:
