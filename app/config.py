7d020a9a07dd9447f3ef538aa9c6fde225cc0fc1
Ruslan <ruslan.grudistov@gmail.com>
2025-02-05 15:21:35 +0200

    conf

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env"))
 
